e710b1e8686367e1c660b341f1d74a8f3fa4a557
dawn-cmd <hjing7@wisc.edu>
2023-03-23 16:22:12 -0500

    test chatgpt

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,14 +1,15 @@
-use minigrep::Config;
-use std::{self, env, process};
+use minigrep::Config; // Import the Config struct from the minigrep module
+use std::{env, process}; // Import standard Rust modules
+
 fn main() {
-    let config = Config::new(env::args()).unwrap_or_else(|err| {
-        eprintln!("Problem parsing arguments: {err}");
-        process::exit(1);
-    });
-    // println!("Search for {}", config.query);
-    // println!("In file {}", config.file_path);
-    if let Err(e) = minigrep::run(config) {
-        eprintln!("Application Error: {}", e);
-        process::exit(1);
+    let config = Config::new(env::args()) // Create a new Config object from command line arguments
+        .unwrap_or_else(|err| { // If there was an error creating the object, handle it
+            eprintln!("Problem parsing arguments: {err}"); // Print the error message to standard error
+            process::exit(1); // Exit the program with an error code
+        });
+    
+    if let Err(e) = minigrep::run(config) { // If there was an error running the minigrep::run function with the config object
+        eprintln!("Application Error: {}", e); // Print the error message to standard error
+        process::exit(1); // Exit the program with an error code
     }
 }
