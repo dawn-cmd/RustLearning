077dde21c0df06e11beedd47e66e54f87f8339ad
dawn-cmd <hjing7@wisc.edu>
2023-03-22 20:56:32 -0500

    test udpate

---
+++ minigrep/target/debug/deps/minigrep-fe07f7be11d44724.d
@@ -0,0 +1,7 @@
+D:\OneDrive - UW-Madison\Code\RustLearning\minigrep\target\debug\deps\minigrep-fe07f7be11d44724.rmeta: src\lib.rs
+
+D:\OneDrive - UW-Madison\Code\RustLearning\minigrep\target\debug\deps\libminigrep-fe07f7be11d44724.rlib: src\lib.rs
+
+D:\OneDrive - UW-Madison\Code\RustLearning\minigrep\target\debug\deps\minigrep-fe07f7be11d44724.d: src\lib.rs
+
+src\lib.rs:
